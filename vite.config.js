c762ad2362737cb7672fe1fabc43094dabda8057
shinyubin1015 <penutljh@gmail.com>
2026-01-31 02:07:33 +0000

    vite.config.js에 build 설정 추가 및 outDir을 "dist/client"로 설정함

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 export default defineConfig({
   appType: "spa", // ✅ 이거 추가 (라우터 직접접속 404 해결)
   plugins: [react(), cloudflare()],
+  build: {
+    outDir: "dist/client",
+  },
   server: {
     proxy: {
       "/api": "http://localhost:8790",
